58fb2aba5522d1cb1f295f4582dc8235c83a4623
santi-giraldo <santyagonicolas2006@gmail.com>
2026-05-13 18:17:28 -0500

    Corrección de CMakeLists, nombre de carpetas y se agrega build de Ubuntu

---
+++ build/CMakeFiles/Inventario.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/santy/PROYECTO-FINAL-ALSE-/DataHub/DBControl.cpp" "CMakeFiles/Inventario.dir/DataHub/DBControl.cpp.o" "gcc" "CMakeFiles/Inventario.dir/DataHub/DBControl.cpp.o.d"
+  "/home/santy/PROYECTO-FINAL-ALSE-/build/Inventario_autogen/mocs_compilation.cpp" "CMakeFiles/Inventario.dir/Inventario_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Inventario.dir/Inventario_autogen/mocs_compilation.cpp.o.d"
+  "/home/santy/PROYECTO-FINAL-ALSE-/compItem/CompForm.cpp" "CMakeFiles/Inventario.dir/compItem/CompForm.cpp.o" "gcc" "CMakeFiles/Inventario.dir/compItem/CompForm.cpp.o.d"
+  "/home/santy/PROYECTO-FINAL-ALSE-/main.cpp" "CMakeFiles/Inventario.dir/main.cpp.o" "gcc" "CMakeFiles/Inventario.dir/main.cpp.o.d"
+  "/home/santy/PROYECTO-FINAL-ALSE-/model/CompList.cpp" "CMakeFiles/Inventario.dir/model/CompList.cpp.o" "gcc" "CMakeFiles/Inventario.dir/model/CompList.cpp.o.d"
+  "/home/santy/PROYECTO-FINAL-ALSE-/model/FiltProxy.cpp" "CMakeFiles/Inventario.dir/model/FiltProxy.cpp.o" "gcc" "CMakeFiles/Inventario.dir/model/FiltProxy.cpp.o.d"
+  "/home/santy/PROYECTO-FINAL-ALSE-/shelf.cpp" "CMakeFiles/Inventario.dir/shelf.cpp.o" "gcc" "CMakeFiles/Inventario.dir/shelf.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
